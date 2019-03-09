44c09d894be17dd27aaecca6be92ae3614069a20
Jason Fiorentino <fiorentino.jason@yahoo.co.id>
2019-03-09 23:15:23 +0700

    Sparepart
    Migration + Model Sparepart

--- app/Sparepart.php
+++ app/Sparepart.php
@@ -7,4 +7,35 @@ use Illuminate\Database\Eloquent\Model;
 class Sparepart extends Model
 {
     //
+    protected $table = 'Sparepart';
+    protected $primaryKey = 'Kode_Sparepart';
+    public $timestamp = true;
+    protected $fillable = [
+        'Kode_Sparepart',
+        'Tipe_Barang',
+        'Nama_Sparepart',
+        'Merk_Sparepart',
+        'Rak_Sparepart',
+        'Jumlah_Sparepart',
+        'Stok_Minimum_Sparepart',
+        'Harga_Beli',
+        'Harga_Jual',
+        'Gambar'
+    ];
+
+    public function detail_pengadaans()
+    {
+        return $this->hasMany('App\Detail_Pengadaan');
+    }
+
+    public function motors()
+    {
+        return $this->hasMany('App\Motor');
+    }
+
+    public function detail_spareparts()
+    {
+        return $this->hasMany('App\Detail_Sparepart');
+    }
+    
 }
--- database/migrations/2019_03_09_160723_create_spareparts_table.php
+++ database/migrations/2019_03_09_160723_create_spareparts_table.php
@@ -14,7 +14,16 @@ class CreateSparepartsTable extends Migration
     public function up()
     {
         Schema::create('spareparts', function (Blueprint $table) {
-            $table->increments('id');
+            $table->string('Kode_Sparepart',10);
+            $table->string('Tipe_Barang',10);
+            $table->string('Nama_Sparepart',50);
+            $table->string('Merk_Sparepart',50);
+            $table->string('Rak_Sparepart',15);
+            $table->integer('Jumlah_Sparepart');
+            $table->integer('Stok_Minimum_Sparepart');
+            $table->double('Harga_Beli');
+            $table->double('Harga_Jual');
+            $table->string('Gambar',255);
             $table->timestamps();
         });
     }
