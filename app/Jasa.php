b00df5ef86c75d7e7bf228dcb0e897092fc8459d
Jason Fiorentino <fiorentino.jason@yahoo.co.id>
2019-03-10 00:57:15 +0700

    Jasa
    Migration + Model Jasa

---
+++ app/Jasa.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Jasa extends Model
+{
+    //
+    protected $table = 'Jasa';
+    protected $primaryKey = 'Id_Jasa';
+    public $timestamp = true;
+    protected $fillable = [
+        'Nama_Jasa',
+        'Harga_Jasa'
+    ];
+
+    public function detail_jasas()
+    {
+        return $this->hasMany('App\Detail_Jasa');
+    }
+}
---
+++ database/migrations/2019_03_09_175424_create_jasas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJasasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jasas', function (Blueprint $table) {
+            $table->increments('Id_Jasa');
+            $table->string('Nama_Jasa',50);
+            $table->double('Harga_Jasa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jasas');
+    }
+}
