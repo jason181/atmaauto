1c4b0793a5b884c72452ee893d1d1e34ea0656eb
Jason Fiorentino <fiorentino.jason@yahoo.co.id>
2019-03-09 22:56:37 +0700

    Konsumen
    Migration + Model Konsumen

---
+++ app/Konsumen.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Konsumen extends Model
+{
+    //
+    protected $table = 'Konsumen';
+    protected $primaryKey = 'Id_Konsumen';
+    public $timestamp = true;
+    protected $fillable = [
+        'Id_Konsumen',
+        'Nama_Konsumen',
+        'Alamat_Konsumen',
+        'Telepon_Konsumen'
+    ];
+
+    public function transaksi_penjualans()
+    {
+        return $this->hasMany('App\Transaksi_Penjualan');
+    }
+
+    public function cabangs()
+    {
+        return $this->hasMany('App\Motor_Konsumen');
+    }
+
+}
---
+++ database/migrations/2019_03_09_155255_create_konsumens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKonsumensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('konsumens', function (Blueprint $table) {
+            $table->increments('Id_Konsumen');
+            $table->string('Nama_Konsumen');
+            $table->string('Telepon_Konsumen');
+            $table->string('Alamat_Konsumen');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('konsumens');
+    }
+}
