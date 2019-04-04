17c1e2a4aebcfc78c89295593e862bc84c24ce6f
Your Name <tlolyelia@gmail.com>
2019-04-04 22:39:05 +0700

    api controller role

---
+++ app/Transformers/RoleTransformers.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use App\Role;
+
+class RoleTransformers extends TransformerAbstract
+{
+    /**
+     * Transform Branch.
+     *
+     * @param Branch $branch
+     */
+    public function transform(Role $role)
+    {
+        return [
+            'Id_Role'           => $role->Id_Role,
+            'Nama_Role'         => $role->Nama_Role,
+        ];
+    }
+}
