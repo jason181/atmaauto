960783a1b63640588cef68ed09a11839d6da9dfb
Jason Fiorentino <fiorentino.jason@yahoo.co.id>
2019-05-11 17:04:55 +0700

    store Detail Sparepart
    store Detail Sparepart semoga fix

--- app/Http/Controllers/DetailSparepartController.php
+++ app/Http/Controllers/DetailSparepartController.php
@@ -8,6 +8,8 @@ use App\Transformers\DetailSparepartTransformers;
 use App\Detail_Sparepart;
 use App\Transaksi_Penjualan;
 use App\Montir;
+use App\Sparepart;
+use App\Transformers\TransaksiPenjualanTransformers;
 
 class DetailSparepartController extends RestController
 {
@@ -22,50 +24,40 @@ class DetailSparepartController extends RestController
 
     public function store(Request $request)
     {
-        // dd($request->Detail_Sparepart[0]['Id_Transaksi']);
-        $detail_sparepart = Detail_Sparepart::where('Id_Transaksi',$request->Detail_Sparepart[0]['Id_Transaksi'])->first();
-        // $detail = $request->Detail_Sparepart;
-        // dd($detail_sparepart->Id_Jasa_Montir);
+
+        $montir = Detail_Sparepart::where('Id_Transaksi',$request->Detail_Sparepart[0]['Id_Transaksi'])->first()->value('Id_Jasa_Montir');
+
         $penjualan  = new Transaksi_Penjualan;
-        // if($request->has('Detail_Sparepart'))
-        // {
-        //     $sparepart      = $request->get('Detail_Sparepart');
-        //     $countsparepart = count($sparepart);
-        //     for($i=0;$i<$countsparepart;$i++)
-        //     {
-        //         $sparepart[$i]['Id_Jasa_Montir']=$montir->Id_Jasa_Montir;
-        //     }
-        // }
+        $detail_sparepart = new Detail_Sparepart;
+
         if($request->has('Detail_Sparepart'))
         {
             $sparepart      = $request->get('Detail_Sparepart');
             $countsparepart = count($sparepart);
             for($i=0;$i<$countsparepart;$i++)
             {
-                $sparepart[$i]['Id_Jasa_Montir']=$detail_sparepart->Id_Jasa_Montir;
+                $detail_sparepart->Id_Transaksi    = $sparepart[$i]['Id_Transaksi'];
+                $detail_sparepart->Id_Jasa_Montir  = $montir;
+                $detail_sparepart->Kode_Sparepart  = $sparepart[$i]['Kode_Sparepart'];
+                $detail_sparepart->Harga_Satuan    = $sparepart[$i]['Harga_Satuan'];
+                $detail_sparepart->Jumlah          = $sparepart[$i]['Jumlah'];
+                $detail_sparepart->Subtotal_Detail_Sparepart = $sparepart[$i]['Subtotal_Detail_Sparepart'];
+                $detail_sparepart->save();
+
+                $sparepartCollection=Sparepart::where('Kode_Sparepart',$sparepart[$i]['Kode_Sparepart'])->get();
+                $sparepartdata=$sparepartCollection->first();
                 
+                $sparepartdata->Jumlah_Sparepart -= $sparepart[$i]['Jumlah'];
+                $sparepartdata->save();
             }
-            // dd($sparepart[0]['Id_Jasa_Montir']);
-            $penjualan = DB::transaction(function () use ($penjualan,$sparepart){
-                $penjualan->detail_spareparts()->createMany($sparepart);
-                return $penjualan;
-            });
 
         }
 
-        // $detail_sparepart = Detail_Sparepart::create([
-        //     'Id_Transaksi'              => $request->Id_Transaksi,
-        //     'Id_Jasa_Montir'            => $montir->Id_Jasa_Montir,
-        //     'Kode_Sparepart'            => $request->Kode_Sparepart,
-        //     'Harga_Satuan'              => $request->Harga_Satuan,
-        //     'Jumlah'                    => $request->Jumlah,
-        //     'Subtotal_Detail_Sparepart' => $request->Subtotal_Detail_Sparepart
-        // ]);
-        $penjualan = Transaksi_Pengadaan::find($request->Id_Transaksi);
+        $penjualan = Transaksi_Penjualan::find($request->Detail_Sparepart[0]['Id_Transaksi']);
         $penjualan->Total += $request->Subtotal_Detail_Sparepart;
         $penjualan->save();
 
-        $response = $this->generateItem($detail_sparepart);
+        $response = $this->generateItem($penjualan, new TransaksiPenjualanTransformers);
         return $this->sendResponse($response);
     }
 
