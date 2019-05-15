24aa30012e2001c6292f64d9ff9d7cc7aad333f8
sumariana <42775471+sumariana@users.noreply.github.com>
2019-05-15 14:22:03 +0700

    update cetak nota lunas

--- resources/views/cetak_nota_lunas.blade.php
+++ resources/views/cetak_nota_lunas.blade.php
@@ -3,11 +3,12 @@
     <title>Atma Auto</title>
         <style>
             .mytable{
-                border:1px solid black;
+                border-top:1px solid black;
+                border-bottom:1px solid black;
             }
             table,th,td{
-                border:1px solid black;
-                border-style:dashed;
+                /* border:1px solid black; */
+                /* border-style:dashed; */
             }
             table {
                 border-collapse: collapse;
@@ -34,131 +35,142 @@
                         <td rowspan="5" colspan="2" style="text-align:right;padding:3px">
                             <img src="{{ public_path('/images/Logo_Transparan.png') }}" alt="Logo_AA" width="120px;">
                         </td>
-                        <td colspan="4" style="text-align:center;font-size:20pt;">
+                        <td colspan="3" style="text-align:center;font-size:20pt;">
                             ATMA AUTO
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="4" style="text-align:center;">
+                        <td colspan="3" style="text-align:center;">
                             MOTORCYCLE SPAREPARTS AND SERVICE
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="4" style="text-align:center;">
+                        <td colspan="3" style="text-align:center;">
                             Jl. Babarsari No. 43 Yogyakarta 552181
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="4" style="text-align:center;">
+                        <td colspan="3" style="text-align:center;">
                             Telp. (0274) 487711
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="4" style="text-align:center;">
+                        <td colspan="3" style="text-align:center;">
                             http://atmauto.jasonfw.com/
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="6" style="border-top:1px solid black;font-weight:bold;text-align:center;padding:5px;">NOTA LUNAS</td>
+                        <td colspan="5" class="mytable" style="font-weight:bold;text-align:center;padding:5px;">Cetak Nota Lunas</td>
                     </tr>
                     <tr>
                         <td></td>
                         <td></td>
                         <td></td>
-                        <td></td>
-                        <td>No :</td>
-                        <td>{{ $datas[0]->Id_Pengadaan }}</td>
+                        <td colspan="2" style="text-align:right;">{{ date('d M Y H:i:s', strtotime($header[0]->created_at)) }}</td>
                     </tr>
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>Tanggal :</td>
-                        <td>{{ date('d M Y', strtotime($datas[0]->Tanggal_Pengadaan)) }}</td>
-                    </tr>
-                    <!-- <tr>
-                        <td colspan="2" rowspan="4">
-                        Kepada Yth: <br>
-                        {{ $datas[0]->Nama_Supplier }} <br>
-                        {{ $datas[0]->Alamat_Supplier }} <br>
-                        {{ $datas[0]->Telepon_Supplier }}
-                        </td>
-                    </tr> -->
-                    <tr>
-                        <td colspan="2" style="border-top:1px solid black; border-left:1px solid black; border-right:1px solid black;; border-bottom:0px; border-style:dashed;padding:3px;">Kepada Yth:</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" style="border-top:0px; border-left:1px solid black; border-right:1px solid black;; border-bottom:0px; border-style:dashed;padding:3px;">{{ $datas[0]->Nama_Supplier }}</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" style="border-top:0px; border-left:1px solid black; border-right:1px solid black;; border-bottom:0px; border-style:dashed;padding:3px;">{{ $datas[0]->Alamat_Supplier }}</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" style="border-top:0px; border-left:1px solid black; border-right:1px solid black;; border-bottom:1px solid black; border-style:dashed;padding:3px;">{{ $datas[0]->Telepon_Supplier}} </td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td colspan="6" style="padding:10px;" >Mohon untuk disediakan barang-barang berikut :</td>
-                    </tr>
-                    <tr class="mytable">
-                        <th class="mytable" style="text-align:center;">No</th>
-                        <th class="mytable" style="text-align:center;">Nama Barang</th>
-                        <th class="mytable" style="text-align:center;">Merk</th>
-                        <th class="mytable" style="text-align:center;">Tipe Barang</th>
-                        <th class="mytable" style="text-align:center;">Satuan</th>
-                        <th class="mytable" style="text-align:center;">Jumlah</th>
-                    </tr>
-                    {{$i=0}}
-                    @foreach($datas as $data)
-                    {{$i++}}
-                    <tr>
-                        <td class="mytable" style="text-align:center;padding:5px;">{{$i}}</td>
-                        <td class="mytable" style="padding:5px;">{{$data->Nama_Sparepart}}</td>
-                        <td class="mytable" style="padding:5px;">{{$data->Merk_Sparepart}}</td>
-                        <td class="mytable" style="padding:5px;">{{$data->Tipe_Barang}}</td>
-                        <td class="mytable" style="text-align:center;padding:5px;">{{$data->Harga_Beli}}</td>
-                        <td class="mytable" style="text-align:center;padding:5px;">{{$data->Jumlah}}</td>
-                    </tr>
-                    @endforeach
-                    <!-- <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>Total Harga :</td>
-                        <td> {{$datas[0]->Total_Harga}} </td>
-                    </tr> -->
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td colspan="2" style="text-align:center;padding-top:25px;padding-bottom:75px;">Hormat Kami,</td>
+                        <td colspan="5" style="font-size:18px;">{{$kode[0]->Kode_Transaksi}}</td>
                     </tr>
                     
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td colspan="2" style="text-align:center;">(Philips Purnomo)</td>
+                        <td>Cust</td>
+                        <td colspan="2">{{$header[0]->Cust}}</td>
+                        <td>CS</td>
+                        <td>{{$cs[0]->CS}}</td>
                     </tr>
+                    <tr>
+                        <td>Telepon</td>
+                        <td colspan="2">{{$header[0]->Telepon}}</td>
+                        <td>Montir</td>
+                        <td>{{$montir}}</td>
+                    </tr>
+                    <tr>
+                        <td style="padding-bottom:15px;">Motor</td>
+                        <td colspan="2" style="padding-bottom:15px;">Merk Motor Jenis Motor Plat Motor</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    @if($s_status == true)
+                    <tr>
+                        <td colspan="5" class="mytable" style="font-size:16px;font-weight:bold;text-align:center;padding:2px;">SPAREPARTS</td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" class="mytable" style="padding:10px;"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" class="mytable" style="padding:1px;"></td>
+                    </tr>
+                    <tr class="mytable">
+                        <th class="mytable" style="text-align:center;">Kode</th>
+                        <th class="mytable" style="text-align:center;">Nama</th>
+                        <th class="mytable" style="text-align:center;">Merk</th>
+                        <th class="mytable" style="text-align:center;">Harga</th>
+                        <th class="mytable" style="text-align:center;">Jumlah</th>
+                        <th class="mytable" style="text-align:center;">Subtotal</th>
+                    </tr>
+                    {{$jumlah=0}}
+                    @foreach($spareparts as $sparepart)
+                    {{$jumlah += $sparepart->Jumlah}}
+                    <tr>
+                        <td> {{$sparepart->Kode}} </td>
+                        <td> {{$sparepart->Nama}} </td>
+                        <td> {{$sparepart->Merk}} </td>
+                        <td> {{$sparepart->Harga}} </td>
+                        <td> {{$sparepart->Jumlah}} </td>
+                        <td style="text-align:right"> {{$sparepart->Subtotal}} </td>
+                    </tr>
+                    @endforeach
+                    <tr>
+                        <td colspan="5" class="mytable" style="padding:1px;"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" style="text-align:right">{{$jumlah}}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" class="mytable" style="padding:1px;"></td>
+                    </tr>
+                    @endif
+                    <tr>
+                        <td colspan="5" style="padding:10px;"></td>
+                    </tr>
+                    @if($j_status == true)
+                    <tr>
+                        <td colspan="5" class="mytable" style="font-size:16px;font-weight:bold;text-align:center;padding:2px;">SERVICES</td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" class="mytable" style="padding:10px;"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" class="mytable" style="padding:1px;"></td>
+                    </tr>
+                    <tr class="mytable">
+                        <th class="mytable" style="padding-left:10px;">Kode</th>
+                        <th class="mytable">Nama</th>
+                        <th class="mytable">Harga</th>
+                        <th class="mytable">Jumlah</th>
+                        <th class="mytable" style="text-align:right;">Sub Total</th>
+                    </tr>
+                    {{$i=0}}
+                    @foreach($jasas as $jasa)
+                    {{$i++}}
+                    <tr>
+                        <td style="padding-left:10px;"> {{$jasa->KodeJasa}} </td>
+                        <td> {{$jasa->NamaJasa}} </td>
+                        <td></td>
+                        <td></td>
+                        <td style="text-align:right">1</td>
+                    </tr>
+                    @endforeach
+                    <tr>
+                        <td colspan="5" class="mytable" style="padding:1px;"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" style="text-align:right"> {{$i}} </td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" class="mytable" style="padding:1px;"></td>
+                    </tr>
+                    @endif
                 </tbody>
             </table>
         </div>
