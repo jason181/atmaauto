ba2044858e64f1fd664930a9e5bbce64e46ae49c
Your Name <tlolyelia@gmail.com>
2019-05-20 20:39:24 +0700

    laporancontroller

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -855,6 +855,8 @@ class LaporanController extends Controller
             return response()->json([
                 'datas' => (bool) $datas,
                 'datas' => $datas,
+                'year' => $year,
+                'tipe' => $tipe,
                 'message' => $datas ? 'Success' : 'Error',
             ]);
         }
