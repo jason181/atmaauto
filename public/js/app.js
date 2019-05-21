8a7de5906775753c0241b1338f03013c4683daf0
sumariana <42775471+sumariana@users.noreply.github.com>
2019-05-21 12:56:23 +0700

    lalala

--- public/js/app.js
+++ public/js/app.js
@@ -90834,8 +90834,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\atmaauto\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\atmaauto\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\SUMA\Documents\GitHub\atmaauto\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\SUMA\Documents\GitHub\atmaauto\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
